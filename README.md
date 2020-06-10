1c4de684172ab183c55e560a1dd3b402092b373b
Jerred Shepherd <shepherdjerred@gmail.com>
2020-06-09 21:37:13 -0700

    Add status

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # EC2 Instance Restart Frontend
+
 [![Build Status](https://travis-ci.org/shepherdjerred/ec2-instance-restart-frontend.svg?branch=master)](https://travis-ci.org/shepherdjerred/ec2-instance-restart-frontend)
 
 A simple React UI for an [EC2 instance restart endpoint](https://github.com/shepherdjerred/ec2-instance-restart).
 
 ## Features
-* Saves secret to local storage
+
+- Saves secret to local storage
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,6 +6,14 @@ interface HomeState {
   response: ApiResponse | null;
   secret: string;
   loading: boolean;
+  status: InstanceStatus;
+  statusAttempt: number;
+  activeButton: Button | null;
+}
+
+enum Button {
+  START,
+  STOP,
 }
 
 interface ApiResponse {
@@ -18,6 +26,19 @@ interface ApiResponseBody {
   details: string;
 }
 
+enum InstanceStatus {
+  UNKNOWN,
+  PENDING,
+  RUNNING,
+  SHUTTING_DOWN,
+  TERMINATED,
+  STOPPING,
+  STOPPED,
+}
+
+const MAX_STATUS_ATTEMPTS = 10;
+
+// This is monstrosity of a React component but it's too late to turn back
 export default class Home extends React.Component<unknown, HomeState> {
   constructor(props: Readonly<unknown>) {
     super(props);
@@ -25,7 +46,14 @@ export default class Home extends React.Component<unknown, HomeState> {
       response: null,
       secret: localStorage.getItem("secret") || "",
       loading: false,
+      status: InstanceStatus.UNKNOWN,
+      statusAttempt: 0,
+      activeButton: null,
     };
+  }
+
+  componentDidMount() {
+    this.getInstanceStatus().then();
   }
 
   handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -35,37 +63,113 @@ export default class Home extends React.Component<unknown, HomeState> {
     });
   };
 
+  getInstanceStatus = async (): Promise<void> => {
+    if (this.state.secret === "") {
+      console.log("Skipping status check since secret is empty");
+      return;
+    }
+
+    const response = await axios.post<ApiResponse>("https://instance-api.shepherdjerred.com/status", {
+      secret: this.state.secret,
+    });
+
+    let status: InstanceStatus;
+    if (typeof response.data.body === "string") {
+      const lastWord: string = response.data.body.split(" ").pop() || "";
+      status = this.convertStatus(lastWord);
+    } else {
+      status = InstanceStatus.UNKNOWN;
+    }
+
+    this.setState({
+      ...this.state,
+      status,
+      statusAttempt: this.state.statusAttempt + 1,
+    });
+
+    if (status === InstanceStatus.UNKNOWN && this.state.statusAttempt < MAX_STATUS_ATTEMPTS) {
+      setTimeout(() => this.getInstanceStatus(), 4000);
+    }
+  };
+
+  convertStatus(status: string): InstanceStatus {
+    switch (status) {
+      case "pending":
+        return InstanceStatus.PENDING;
+      case "running":
+        return InstanceStatus.RUNNING;
+      case "shutting-down":
+        return InstanceStatus.SHUTTING_DOWN;
+      case "terminated":
+        return InstanceStatus.TERMINATED;
+      case "stopping":
+        return InstanceStatus.STOPPING;
+      case "stopped":
+        return InstanceStatus.STOPPED;
+      default:
+        return InstanceStatus.UNKNOWN;
+    }
+  }
+
+  convertEnum(status: InstanceStatus): string {
+    switch (status) {
+      case InstanceStatus.PENDING:
+        return "Pending";
+      case InstanceStatus.RUNNING:
+        return "Running";
+      case InstanceStatus.SHUTTING_DOWN:
+        return "Shutting Down";
+      case InstanceStatus.STOPPED:
+        return "Stopped";
+      case InstanceStatus.STOPPING:
+        return "Stopping";
+      case InstanceStatus.TERMINATED:
+        return "Terminated";
+      case InstanceStatus.UNKNOWN:
+        return "Unknown";
+    }
+  }
+
   startInstance = async (): Promise<void> => {
-    this.beginRequest();
+    if (this.state.activeButton != null) {
+      return;
+    }
+    this.beginRequest(Button.START);
     const response = await axios.post<ApiResponse>("https://instance-api.shepherdjerred.com/start", {
       secret: this.state.secret,
     });
-    this.endRequest(response);
+    await this.endRequest(response);
   };
 
   stopInstance = async (): Promise<void> => {
-    this.beginRequest();
+    if (this.state.activeButton != null) {
+      return;
+    }
+    this.beginRequest(Button.STOP);
     const response = await axios.post<ApiResponse>("https://instance-api.shepherdjerred.com/stop", {
       secret: this.state.secret,
     });
-    this.endRequest(response);
+    await this.endRequest(response);
   };
 
-  beginRequest() {
+  beginRequest(button: Button) {
     localStorage.setItem("secret", this.state.secret);
     this.setState({
       ...this.state,
       response: null,
       loading: true,
+      activeButton: button,
     });
   }
 
-  endRequest(response: AxiosResponse<ApiResponse>) {
+  async endRequest(response: AxiosResponse<ApiResponse>) {
     this.setState({
       ...this.state,
       response: response.data,
       loading: false,
+      activeButton: null,
     });
+    await this.getInstanceStatus();
   }
 
   render() {
@@ -75,12 +179,17 @@ export default class Home extends React.Component<unknown, HomeState> {
           className={classNames({
             notification: true,
             "is-danger": this.state.response.statusCode >= 400,
-            "is-success": this.state.response.statusCode < 400,
+            "is-primary": this.state.response.statusCode < 400,
           })}
         >
           {this.state.response.body}
         </div>
       ) : null;
+
+    const instanceStatus =
+      this.state.status === InstanceStatus.UNKNOWN ? null : (
+        <div className="notification">{"Instance Status: " + this.convertEnum(this.state.status)}</div>
+      );
 
     return (
       <div>
@@ -96,6 +205,7 @@ export default class Home extends React.Component<unknown, HomeState> {
             <div className="columns">
               <div className="column is-one-third is-offset-one-third">
                 {notification}
+                {instanceStatus}
                 <div className="field">
                   <div className="control">
                     <input
@@ -113,9 +223,10 @@ export default class Home extends React.Component<unknown, HomeState> {
                       onClick={this.startInstance}
                       className={classNames({
                         button: true,
-                        "is-primary": true,
-                        "is-loading": this.state.loading,
+                        "is-success": true,
+                        "is-loading": this.state.loading && this.state.activeButton === Button.START,
                       })}
+                      disabled={this.state.loading && this.state.activeButton != Button.START}
                     >
                       Start
                     </button>
@@ -126,8 +237,9 @@ export default class Home extends React.Component<unknown, HomeState> {
                       className={classNames({
                         button: true,
                         "is-danger": true,
-                        "is-loading": this.state.loading,
+                        "is-loading": this.state.loading && this.state.activeButton === Button.STOP,
                       })}
+                      disabled={this.state.loading && this.state.activeButton != Button.STOP}
                     >
                       Stop
                     </button>
